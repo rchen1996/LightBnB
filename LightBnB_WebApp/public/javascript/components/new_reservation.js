718be8264ed5b78c965f25ad8053fa9bbcf18908
Rebecca Chen <rebecca061996@gmail.com>
2021-02-04 17:10:41 +0000

    create reservation form input

---
+++ LightBnB_WebApp/public/javascript/components/new_reservation.js
@@ -0,0 +1,11 @@
+$(() => {
+
+  const $reservationForm = $(`
+    <label for="start">Start Date:</label>
+    <input type="date" id="start" name="reservation-start">
+
+    <label for="end">End Date:</label>
+    <input type="date" id="start" name="reservation-end">
+  `);
+      
+});
