84228794add91a6204c482c602840b9039ed0000
Rebecca Chen <rebecca061996@gmail.com>
2021-02-04 16:54:10 +0000

    create route for reservation post request

--- LightBnB_WebApp/server/apiRoutes.js
+++ LightBnB_WebApp/server/apiRoutes.js
@@ -35,5 +35,17 @@ module.exports = function(router, database) {
       });
   });
 
+  router.post('/reservations', (req, res) => {
+    const userId = req.session.userId;
+    database.addReservation({...req.body, guest_id: userId})
+      .then(reservation => {
+        res.send(property);
+      })
+      .catch(e => {
+        console.error(e);
+        res.send(e)
+      });
+  })
+
   return router;
 }
