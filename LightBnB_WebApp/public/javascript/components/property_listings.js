1513ddf9f7453021265d58b9ab29a9a61bdabb1f
Rebecca Chen <rebecca061996@gmail.com>
2021-02-05 00:34:53 +0000

    update reservation form handling so that it shows user's reservations rather than all listings after submitting a reservation

--- LightBnB_WebApp/public/javascript/components/property_listings.js
+++ LightBnB_WebApp/public/javascript/components/property_listings.js
@@ -35,7 +35,13 @@ $(() => {
         const data = $(this).serialize();
         submitReservation(data)
           .then(() => {
-            views_manager.show('listings');
+            propertyListings.clearListings();
+            getAllReservations()
+              .then(function(json) {
+                propertyListings.addProperties(json.reservations, true);
+                views_manager.show('listings');
+              })
+              .catch(error => console.error(error));
           })
           .catch((error) => {
             console.error(error);
